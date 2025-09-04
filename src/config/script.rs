f88fd848c40f0246af7af31b76bce713b3a10cf0
NANASE <136774049+wszhdshys@users.noreply.github.com>
2025-09-04 10:58:57 +0800

    fix: user_config (#29)
    1，修改了配置文件相关，将可被用户修改的配置信息移到了savedata文件夹下的user.toml。
    2，添加了build函数以便使用。
    3，更新了文档。

--- src/config/script.rs
+++ src/config/script.rs
@@ -1,37 +1,23 @@
-use crate::config::voice::deserialize_duration_from_secs;
-use crate::config::ENGINE_CONFIG;
+use crate::config::voice::{deserialize_duration_from_secs, serialize_duration_as_secs};
 use serde::{Deserialize, Serialize};
-use std::fs;
 use tokio::time::Duration;
 
-lazy_static::lazy_static! {
-    pub static ref SCRIPT_CONFIG: ScriptConfig = load_script_config();
-}
-
 #[derive(Debug, Deserialize, Serialize)]
-pub struct ScriptConfig {
-    auto: AutoConfig,
-}
-
-impl ScriptConfig {
-    pub fn delay(&self) -> Duration {
-        self.auto.delay
-    }
-
-    pub fn is_wait(&self) -> bool {
-        self.auto.is_wait
-    }
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct AutoConfig {
-    #[serde(deserialize_with = "deserialize_duration_from_secs")]
+pub struct AutoConfig {
+    #[serde(
+        deserialize_with = "deserialize_duration_from_secs",
+        serialize_with = "serialize_duration_as_secs"
+    )]
     delay: Duration,
     is_wait: bool,
 }
 
-fn load_script_config() -> ScriptConfig {
-    let content =
-        fs::read_to_string(format!("{}/auto.toml", ENGINE_CONFIG.script_path(),)).unwrap();
-    toml::from_str(&content).unwrap()
+impl AutoConfig {
+    pub fn delay(&self) -> Duration {
+        self.delay
+    }
+
+    pub fn is_wait(&self) -> bool {
+        self.is_wait
+    }
 }
