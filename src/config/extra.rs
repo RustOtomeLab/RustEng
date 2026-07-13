91009d2a1b8de40a59a6a9850216836dc30d4f9e
NANASE <136774049+wszhdshys@users.noreply.github.com>
2026-07-13 20:18:07 +0800

    feat: pagination on the extra view and save&load view (#58)
    实现了存档读档，以及鉴赏界面的分页，同时也支持了更多数量的存档和CG

--- src/config/extra.rs
+++ src/config/extra.rs
@@ -1,16 +1,19 @@
-use crate::config::{cg::CgConfig, ENGINE_CONFIG};
+use crate::config::cg::CG_CONFIG;
+use crate::config::{cg::CgMap, ENGINE_CONFIG};
 use crate::error::{EngineError, SaveError};
 use crate::executors::executor::Executor;
 use serde::{Deserialize, Serialize};
+use std::cell::RefCell;
 use std::fs;
+use std::rc::Rc;
 
 #[derive(Debug, Deserialize, Serialize)]
 pub(crate) struct ExtraConfig {
-    cg: CgConfig,
+    cg: CgMap,
 }
 
 impl ExtraConfig {
-    pub(crate) fn cg(&self) -> u64 {
+    pub(crate) fn cg(self) -> Vec<u64> {
         self.cg.cg()
     }
 }
@@ -23,14 +26,21 @@ impl Executor {
 }
 
 fn load_extra_config() -> ExtraConfig {
-    let content = fs::read_to_string(format!("{}/extra.toml", ENGINE_CONFIG.save_path())).unwrap();
-    toml::from_str(&content).unwrap()
+    if let Ok(content) = fs::read_to_string(format!("{}/extra.toml", ENGINE_CONFIG.save_path())) {
+        toml::from_str(&content).unwrap()
+    } else {
+        let num = CG_CONFIG.length() / 64 + 1;
+        ExtraConfig {
+            cg: CgMap::new(vec![0; num]),
+        }
+    }
 }
 
-pub(crate) fn save_extra_config(cg: u64) -> Result<(), EngineError> {
+pub(crate) fn save_extra_config(cg: Rc<RefCell<Vec<u64>>>) -> Result<(), EngineError> {
+    let cg = cg.borrow();
     let path = format!("{}/extra.toml", ENGINE_CONFIG.save_path());
     let content = toml::to_string(&ExtraConfig {
-        cg: CgConfig::new(cg),
+        cg: CgMap::new(cg.clone()),
     })
     .map_err(SaveError::from)?;
     fs::write(&path, content).map_err(|e| SaveError::Write { path, source: e })?;
